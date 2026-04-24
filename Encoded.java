d467b89d8566270c0b853eb949f80c21e345fd63
Nurul-Syahmina <102904@siswa.unimas.my>
2026-04-25 02:16:53 +0800

    Update Encoded.java

--- Encoded.java
+++ Encoded.java
@@ -5,7 +5,7 @@ public class Encoded {
     private String resultText;
     private final String groupID = "G04/SE-G11";
 
-    // Constructors (Person 4) - test
+    // Constructors (Person 4)
     public Encoded() {}
 
     public Encoded(String inputText) {
